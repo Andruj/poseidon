dfdf07eaa74a5e7a849f942f805ff6d87deec7c1
Andrew Peterson <andrewwaynepeterson@gmail.com>
2017-02-08 19:01:00 -0800

    Added Materialize for the project.

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -4,15 +4,12 @@
 import 'package:angular2/core.dart';
 import 'package:angular2_components/angular2_components.dart';
 
-import 'package:poseidon/hello_dialog/hello_dialog.dart';
-
 @Component(
-  selector: 'my-app',
+  selector: 'app',
   styleUrls: const ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: const [materialDirectives, HelloDialog],
+  directives: const [materialDirectives],
   providers: const [materialProviders],
 )
 class AppComponent {
-  // Nothing here yet. All logic is in HelloDialog.
 }
