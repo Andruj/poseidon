daf7df8181d6c7f298c12a3b40c9c8ce6341f2f5
Andrew Peterson <andrewwaynepeterson@gmail.com>
2017-04-12 15:24:02 -0700

    Added basic function.

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ var functions = require('firebase-functions');
  // Create and Deploy Your First Cloud Functions
  // https://firebase.google.com/docs/functions/write-firebase-functions
 
- exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
- });
+exports.onWatchlistAdded = functions.database.ref('/users/{userId}/regions/{regionId}/watchlist')
+    .onWrite(event => {
+        console.log(event.data.val());
+    });
