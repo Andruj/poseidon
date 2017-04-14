300db0a898780b243b94bb17e062b64a96fc6c8b
Andrew Peterson <andrewwaynepeterson@gmail.com>
2017-04-13 20:57:45 -0700

    Reconfigured project, started building other remove server.

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-pub build
-rm -rf ../andruj.github.io/*
-cp -R build/web/* ../andruj.github.io/
-cd ../andruj.github.io
-git add --all
-git commit -am 'New deploy.'
-git push
-cd ../poseidon
--- functions/watchlist-added.js
+++
@@ -1,7 +0,0 @@
-const functions = require('firebase-functions');
-
-module.exports = functions.database.ref('/users/{userId}/regions/{regionId}/watchlist/{watchlistId}')
-    .onWrite(event => {
-        console.log(event.data.val());
-        return true;
-    });
